0a9f119e6ae0fc2a5034141ad8e19d1371023490
Diego Vicente-Sastre <80335147+BioChess@users.noreply.github.com>
2025-04-03 11:58:39 +0200

    Update 01_Interactive_map.R

--- 01_Interactive_map.R
+++ 01_Interactive_map.R
@@ -8,43 +8,57 @@ gps.df <- read.csv("gps_data.csv")
 # Convertir la columna de fecha a formato adecuado
 gps.df$datetimeGMT <- as.POSIXct(gps.df$datetimeGMT, format="%Y-%m-%d %H:%M:%S", tz="GMT")
 
+# Filtrar datos de los últimos 2 días
+lst_days <- Sys.time() - (2 * 24 * 60 * 60)  # Hace 2 días
+gps.df2 <- gps.df %>% filter(datetimeGMT >= lst_days)
+
 # Crear la paleta de colores para cada birdID
-paleta_colores <- colorFactor(palette = "Set1", domain = gps.df$birdID)
+pal.colors <- colorFactor(palette = "Set1", domain = gps.df$birdID)
 
 # Crear el mapa base
 leafletOptions <- leaflet::leafletOptions(preferCanvas = TRUE)
-mapa <- leaflet(options = leafletOptions) %>%
+map <- leaflet(options = leafletOptions) %>%
   addTiles(options = tileOptions(maxZoom = 10))
 
 # Agregar capas de tracks por cada individuo
 grupos <- unique(gps.df$birdID)  # Obtener los ID únicos
 
 for (bird in grupos) {
-  datos_individual <- gps.df %>% filter(birdID == bird)  # Filtrar datos por birdID
-  
-  mapa <- mapa %>%
+  gps.ind <- gps.df %>% filter(birdID == bird)  # Filtrar datos por birdID
+  lst_pos <- gps.ind %>% filter(datetimeGMT == max(datetimeGMT))
+  map <- map %>%
     addPolylines(
       lng = ~longitude, lat = ~latitude, 
-      data = datos_individual,
+      data = gps.ind,
       color = 'darkgrey',
       weight = 0.75, opacity = 0.7,
       group = bird  # Asigna el track al mismo grupo
     ) %>%
     addCircleMarkers(
       lng = ~longitude, lat = ~latitude, 
-      data = datos_individual,
-      radius = 0.5, color = ~paleta_colores(birdID), 
-      popup = ~paste("ID:", birdID, "<br>Date:", datetimeGMT),
+      data = gps.ind,
+      radius = 0.5,
+      color = ~pal.colors(birdID), 
+      popup = ~paste("ID:", birdID, "<br>Date:", datetimeGMT, "Sex:", sex),
       group = bird  # Asigna un grupo con el nombre del birdID
-    ) 
+    ) %>% 
+    addAwesomeMarkers(
+      lng = ~longitude, lat = ~latitude,
+      data = lst_pos,
+      icon = awesomeIcons(
+        icon = 'star', library = 'fa', markerColor = 'red'
+      ),
+      popup = ~paste("ID:", birdID, "<br>Fecha:", datetimeGMT, "Sex:", sex),
+      group = bird
+    )
 }
 
 # Agregar control de capas para activar/desactivar individuos
-mapa <- mapa %>%
+map <- map %>%
   addLayersControl(
     overlayGroups = grupos,  # Usa los birdID como grupos de control
     options = layersControlOptions(collapsed = FALSE)  # Mostrar la lista expandida
   )
 
 # Guardar el mapa como archivo HTML
-saveWidget(mapa, file = "docs/index.html", selfcontained = FALSE)
+saveWidget(map, file = "docs/index.html", selfcontained = FALSE)
